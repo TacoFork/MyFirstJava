8e9bf145073af5f2ffefe4ea8495c70b497bd788
TacoFork <davidkim654@gmail.com>
2021-07-06 16:16:00 -0400

    first commit

---
+++ src/MyFirstJava.java
@@ -0,0 +1,9 @@
+/*
+  This is the first Java program which will print out a message, Hello World.
+ */
+public class MyFirstJava {
+    public static void main (String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
